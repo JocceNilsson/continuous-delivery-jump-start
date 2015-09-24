90ee986cb9847ffca5bb57824c3bf18732c88480
Joachim Nilsson <Joachim.Nilsson@molybden.se>
2015-09-24 20:36:43 +0200

    Ignored a new test, logging it

---
+++ src/test/java/org/gradle/sample/dummy/DummyTest.java
@@ -0,0 +1,13 @@
+package org.gradle.sample.dummy;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	@Ignore
+	public void dummyTest() {
+		System.out.println("IGNORED???");
+	}
+}
